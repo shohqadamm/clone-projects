2a7f05e886a2847041c5ed319e52db0df767c0e0
Shohqadamm <ergashevshohqadam@gmail.com>
2022-03-15 13:46:24 +0500

    finished

--- startbucks-clone/assets/js/scritpts.js
+++ startbucks-clone/assets/js/scritpts.js
@@ -1,7 +1,10 @@
 const menuBtn = document.getElementById('menu-btn')
+const mobileMenu = document.getElementById('menu')
 
 function navToggle(){
 menuBtn.classList.toggle('open')
+mobileMenu.classList.toggle('hidden')
+document.body.classList.toggle('no-scroll')
 }
 
 menuBtn.addEventListener('click', navToggle)
